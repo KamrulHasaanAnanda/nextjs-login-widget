d0218f44b869b1f8be5cec503d03fbbb4ccbe24a
ananda hasan <ananda@shadhinmusic.com>
2024-09-11 13:31:19 +0600

    react added

--- src/components/AuthWrapper.jsx
+++ src/components/AuthWrapper.jsx
@@ -1,5 +1,6 @@
 "use client"
-import { useState } from 'react';
+
+import React, { useState } from 'react';
 import LoginComponent from './LoginComponent';
 import DisplayComponent from './DisplayComponent';
 
